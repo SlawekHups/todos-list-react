7c053bfefd78da89e69a35dfe7a493e0824a38d2
SlawekHupka <slawek@hupka.pl>
2023-07-19 16:07:46 +0200

    code formatting

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -3,16 +3,16 @@ import "./style.css";
 const Tasks = ({tasks, hideDoneTasks}) => (
     <ul className="tasks ">
         {tasks.map((task) => (
-            <li className={`tasks__item ${task.done && hideDoneTasks ? " tasks__item--hidden" : ""}`} key={task.id}>
-                <button className="tasks__button tasks__button--done">
-             {task.done ? "✔" : ""}
-           </button>
-                <span className={`tasks__content ${task.done ? "tasks__content--done" : ""}`}>
-              {task.content}
-           </span>
-           <button className="tasks__button tasks__button--remove">
-               🗑
-           </button>
+            <li className={`tasks__item ${task.done && hideDoneTasks ? "tasks__item--hidden" : ""}`} key={task.id}>
+              <button className="tasks__button tasks__button--done">
+                {task.done ? "✔" : ""}
+              </button>
+              <span className={`tasks__content ${task.done ? "tasks__content--done" : ""}`}>
+                {task.content}
+              </span>
+              <button className="tasks__button tasks__button--remove">
+                🗑
+              </button>
             </li>
         ))}
     </ul>
