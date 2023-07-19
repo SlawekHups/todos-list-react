d8b361e189fa59e3f395bb736c1cc0ab0c3609c6
SlawekHupka <slawek@hupka.pl>
2023-07-19 15:03:59 +0200

    form class removed

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 
 const Form = () => (
-    <form className="form form__content ">
+    <form className="form">
         <input className="form__field " placeholder="Co jest do zrobienia ?" autoFocus />
         <button className="form__button">Dodaj zadanie</button>
     </form>
